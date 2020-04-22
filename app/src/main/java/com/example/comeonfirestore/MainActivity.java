d8c6ef126c9077ba3b087f6d3ccc69e704b96237
niranjan-magare1997 <47626938+niranjan-magare1997@users.noreply.github.com>
2020-04-22 22:56:39 +0530

    Mobile SMS authentication implemented. You will just get an otp and it will be  automatically checked.

--- app/src/main/java/com/example/comeonfirestore/MainActivity.java
+++ app/src/main/java/com/example/comeonfirestore/MainActivity.java
@@ -12,8 +12,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -23,17 +26,19 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public String TAG = "MainActivity";
-    public Button addData,getData,signUp,signIn;
-    public EditText getName,getSurname;
+    public Button addData,getData,signUp,signIn,sendOtp;
+    public EditText getName,getSurname,getEmail,getPassword,getPhoneNo;
     public String name,surname;
     public String NAME_KEY = "Name";
     public String SURNAME_KEY = "Surname";
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseAuth mAuth;
+    private PhoneAuthProvider phoneAuthProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,20 +55,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             final String TAG = "MainActivity | oncLick ";
             Log.d(TAG, "In function onClick");
 
+            phoneAuthProvider = PhoneAuthProvider.getInstance();
             mAuth = FirebaseAuth.getInstance();
 
-            addData = findViewById(R.id.Add_Data);
-            getData = findViewById(R.id.Get_Data);
-            signIn = findViewById(R.id.signIn);
-            signUp = findViewById(R.id.signUp);
+            addData = findViewById(R.id.Add_Data_Button);
+            getData = findViewById(R.id.Get_Data_Button);
+            signIn = findViewById(R.id.signIn_Button);
+            signUp = findViewById(R.id.signUp_Button);
+            sendOtp = findViewById(R.id.sendOtp_Button);
 
-            getName = findViewById(R.id.Name);
-            getSurname = findViewById(R.id.Surname);
+            getName = findViewById(R.id.NameEditText);
+            getSurname = findViewById(R.id.SurnameEditText);
+            getEmail = findViewById(R.id.emailEditText);
+            getPassword = findViewById(R.id.passwordEditText);
+            getPhoneNo = findViewById(R.id.phoneEditText);
 
             addData.setOnClickListener(MainActivity.this);
             getData.setOnClickListener(MainActivity.this);
             signIn.setOnClickListener(MainActivity.this);
             signUp.setOnClickListener(MainActivity.this);
+            sendOtp.setOnClickListener(MainActivity.this);
         }catch (Exception e){
             Log.e(TAG, "Exception in setComponentsValues function => \n" + e.toString() );
         }
@@ -93,21 +104,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             surname = getSurname.getText().toString();
 
             switch(v.getId()){
-                case R.id.Add_Data:
+                case R.id.Add_Data_Button:
                     Toast.makeText(this, "Add_Data Button Clicked", Toast.LENGTH_SHORT).show();
                     addData();
                     break;
-                case R.id.Get_Data:
+                case R.id.Get_Data_Button:
                     Toast.makeText(this, "Get Data Button Clicked", Toast.LENGTH_SHORT).show();
                     getData();
                     break;
-                case R.id.signIn:
+                case R.id.signIn_Button:
                     Toast.makeText(this, "Sign In Button Clicked", Toast.LENGTH_SHORT).show();
                     signInUser();
                     break;
-                case R.id.signUp:
+                case R.id.signUp_Button:
                     Toast.makeText(this, "Sign Up Button Clicked", Toast.LENGTH_SHORT).show();
                     signUpUser();
+                    break;
+                case R.id.sendOtp_Button:
+                    Toast.makeText(this, "Send OTP Button Clicked", Toast.LENGTH_SHORT).show();
+                    sendOtp();
                     break;
                 default:
                     Toast.makeText(this, "This is default case", Toast.LENGTH_SHORT).show();
@@ -116,6 +131,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }catch (Exception e){
             Log.e(TAG, "Exception in onClick function => \n" + e.toString() );
         }
+    }
+
+    private void sendOtp() {
+        String phNo = getPhoneNo.getText().toString();
+        makeToast("Number  : " + phNo);
+        phoneAuthProvider.verifyPhoneNumber(phNo.trim().toString(), 60L, TimeUnit.SECONDS, this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                makeToast("OTP Sent to number"+s);
+                Log.d(TAG, "onCodeSent: "+s);
+                super.onCodeSent(s, forceResendingToken);
+            }
+
+            @Override
+            public void onCodeAutoRetrievalTimeOut(@NonNull String s) {
+                super.onCodeAutoRetrievalTimeOut(s);
+                makeToast("Auto Retrieval Timeout");
+            }
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                makeToast("Verification Completed");
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                makeToast("Verification Failed");
+                Log.d(TAG, "onVerificationFailed: "+e.toString());
+            }
+        });
     }
 
     private void addData() {
@@ -179,10 +224,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             for (int i = 0; i < stringArray.length; i++) {
                 Log.d(TAG, "Integer Array: " + stringArray[i]);
             }
-            Log.d(TAG, "Integer Array: "+listToArray(integerList));
+            //Log.d(TAG, "Integer Array: "+listToArray(integerList));
 
             db.collection("Whole_Hotel")
-                    //.whereEqualTo("Name","fddf")
+                    //.whereEqualTo("Name","fddf")                              //QUERY//
                     .get()
                     .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                         @Override
@@ -239,8 +284,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void signUpUser(){
-        String email = getName.getText().toString();
-        String password = getSurname.getText().toString();
+        String email = getEmail.getText().toString();
+        String password = getPassword.getText().toString();
 
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
