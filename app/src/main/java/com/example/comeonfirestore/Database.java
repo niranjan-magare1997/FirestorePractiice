5e08956c82c1226b22789cf8b53606e27be354e0
niranjan-magare1997 <47626938+niranjan-magare1997@users.noreply.github.com>
2020-05-10 22:25:09 +0530

    1) Employee Id is auto generated and name and surname is get stored.

--- app/src/main/java/com/example/comeonfirestore/Database.java
+++ app/src/main/java/com/example/comeonfirestore/Database.java
@@ -6,10 +6,12 @@ import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
 import java.util.Map;
+import java.util.Random;
 
 public class Database {
     private FirebaseFirestore db;
@@ -20,27 +22,69 @@ public class Database {
         db = FirebaseFirestore.getInstance();
     }
 
-    public void addEmployee(Map<String,Object> map){
+    private Integer generateNewId () {
+        final int min = 0;
+        final int max = 9;
+        final int random = new Random().nextInt((max - min) + 1) + min;
+        Log.d(TAG, "generateNewId | Random Number Generated => " + random);
+        return  random;
+    }
+
+    public void addEmployee(final Map<String,Object> map){
         try{
             Log.d(TAG, "addEmployee | In function addEmployee() ");
+            final String newEmpId = "EMP_" + generateNewId().toString();
 
-            db.collection("Employee").document("someEmpId")
-                    /**SetOptions.merge() will work same as update() method,
-                     *but if collection or document is not exist this will create them and then insert the data in it*/
-                    .set(map, SetOptions.merge())
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Log.d(TAG, "addEmployee | Employee Added Successful");
+            db.collection("Employee").document(newEmpId)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if(documentSnapshot.exists() == true){
+                            Log.d(TAG, "addEmployee | Employee already exist with the same Id");
+                            String name = documentSnapshot.getString("Name");
+                            String surname = documentSnapshot.getString("Surname");
+                            Log.d(TAG, "addEmployee | Name: "+name+" Surname: "+surname);
+                        }else{
+                            Log.d(TAG, "addEmployee | Employee Not exist already");
+
+                            /**
+                             * SetOptions.merge() will work same as update() method,
+                             *but if collection or document is not exist this will create them and then insert the data in it
+                             * */
+
+                            db.collection("Employee").document(newEmpId)
+                                .set(map, SetOptions.merge())
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(TAG, "addEmployee | Added new Employee");
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.e(TAG, "addEmployee | Failed to add new Employee");
+                                }
+                            });
                         }
-                    }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Log.e(TAG, "addEmployee | Employee Adding Failed......");
-                }
-            });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "addEmployee | Failed to check Employee exist or not");
+                    }
+                });
         }catch(Exception e){
             Log.e(TAG, "addEmployee | Exception in addEmployee: "+e.getMessage());
+        }
+    }
+
+    public void deleteEmployee(String empId){
+        try{
+
+        }catch (Exception e){
+
         }
     }
 
